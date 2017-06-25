49467651fc82a82279315167e65df3bdb2a7595d
Yunsik Park <space214@gmail.com>
2017-06-26 01:28:43 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리팩터링

--- django_app/lists/tests.py
+++ django_app/lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 
 # home_page는 어떤 함수일까 곧 작성하게 될 뷰 함수로 HTML을 반환한다.
@@ -28,4 +29,10 @@ class HomePageTest(TestCase):
         # 반환내용의 <title>태그에 "To-Do lists"라는 단어가 있는지 확인한다
         # 앞선 기능 테스트에서 확인한것이기 때문에 단위테스트도 확인해주어야 한다.
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
