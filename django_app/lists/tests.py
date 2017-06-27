68591b36ac1465bb1879550a32888a08c7192c9f
Yunsik Park <space214@gmail.com>
2017-06-28 00:30:25 +0900

    Item model 추가 및 migrations 연계

--- django_app/lists/tests.py
+++ django_app/lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
+from lists.models import Item
 
 # home_page는 어떤 함수일까 곧 작성하게 될 뷰 함수로 HTML을 반환한다.
 # import 처리부분을 보면 알 수 있지만 이 함수를 lists/view.py파일에 저장할 계획이다.
@@ -61,3 +62,22 @@ class HomePageTest(TestCase):
             re.sub(self.pattern_input_csrf, '', response.content.decode()),
             re.sub(self.pattern_input_csrf, '', expected_html)
         )
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
